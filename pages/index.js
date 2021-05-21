6bf728041d4c9e39c1b799890959eedb9e268f22
robbo23 <robbo1995@gmail.com>
2021-05-21 02:03:26 +0200

    assets

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,6 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
