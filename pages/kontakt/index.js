bf2715c6fccf3826de90dcd0de17a59735788ff4
Igor Wysocki <narghar@gmail.com>
2021-05-27 13:56:01 +0200

    Add contact page

---
+++ pages/kontakt/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function Contact() {
+  return (
+    <div>
+      <Head>
+        <title>Kontakt</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="bg-green-500 md:bg-red-500 lg:bg-teal-500">FORMULARZ KONTAKTOWY</div>
+    </div>
+  );
+}
+
+export default Contact;
