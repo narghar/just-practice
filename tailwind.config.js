3b26916a0d9c068b173c9fc2c7d0400ec9efe963
Igor Wysocki <narghar@gmail.com>
2021-05-27 08:34:54 +0200

    Add custom font, colors, font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,34 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Josefin Sans"],
+    },
+    fontSize: {
+      sm: "20px",
+      base: "40px",
+      lg: "90px",
+      xl: "13.5vw",
+      max: "260px"
+    },
+
+    extend: {
+      screens: {
+        "3xl": "1920px",
+      },
+      colors: {
+        primary: {
+          heading: "#08132c",
+          subheading: "#a6a6a6",
+          link: "#fff",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
