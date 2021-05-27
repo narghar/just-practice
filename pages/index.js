9957f3440579f3906cbc9424637117f31845cbab
Igor Wysocki <narghar@gmail.com>
2021-05-27 13:59:23 +0200

    Create main page layout

--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,119 @@
-export default function Home() {
+import Head from 'next/head';
+import Image from "next/image";
+import Link from "next/link";
+
+function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+    <div>
+      <Head>
+        <title>Ilona Brzozowska</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div className=" max-w-screen-4xl 4xl:mx-auto">
+        <nav>
+          <ul className="flex justify-end">
+            <li>
+              <Link href="/">
+                <a className="text-sm hover:text-primary-subheading">Główna</a>
+              </Link>
+            </li>
+            <li className="text-sm hover:text-primary-subheading mx-5">
+              <Link href="/kontakt">
+                <a>Kontakt</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+        <header className="flex flex-wrap-reverse lg:flex-nowrap justify-center lg:justify-start">
+          <div className="w-full flex-grow relative max-w-4xl px-20 pt-0 lg:pt-20 xl:px-30 xl:pt-30 2xl:px-40 2xl:pt-36">
+            <Image
+              className="z-10 "
+              src="/assets/kolorystka1.png"
+              alt="Zdjęcie Ilona Brzozowska"
+              layout="responsive"
+              width={561}
+              height={748}
+            />
+            <div className="absolute h-1/4 w-full z-0 bottom-0 left-0  gradient-box"></div>
+          </div>
+          <div className="font-sans lg:pt-36 lg:-ml-64 z-20">
+            <h1 className="font-thin text-xl  4xl:text-max text-primary-heading text-center lg:text-left">
+              Kolorystka
+            </h1>
+            <p className="font-thin text-base z-30 text-primary-subheading text-center 2xl:text-left 2xl:ml-48 2xl:-mt-24">
+              Ilona Brzozowska
             </p>
-          </a>
+            <h2 className="font-light text-center xl:text-right 2xl:m-20    text-base md:text-lg text-primary-heading">
+              Styl, kolor, biznes{" "}
+            </h2>
+          </div>
+        </header>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+        <div className="relative flex items-center -mt-24">
+          <figure className="z-20 mx-4">
+            <Image
+              src="/assets/kolorystka2.png"
+              alt="Zdjęcie Ilona Brzozowska"
+              layout="intrinsic"
+              width={475}
+              height={348}
+            />
+          </figure>
+          <figure className="z-20 mx-4">
+            <Image
+              src="/assets/kolorystka3.png"
+              alt="Zdjęcie Ilona Brzozowska"
+              layout="intrinsic"
+              width={705}
+              height={496}
+            />
+          </figure>
+          <figure className="z-20 mx-4 xl:-mt-40">
+            <Image
+              src="/assets/kolorystka4.png"
+              alt="Zdjęcie Ilona Brzozowska"
+              layout="intrinsic"
+              width={443}
+              height={443}
+            />
+          </figure>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className="hidden lg:block absolute h-1/2 w-1/2 z-10 bottom-10 right-0 gray-box"></div>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        <div className="relative grid grid-col-1 lg:grid-cols-3 max-w-screen-3xl mt-44 justify-center">
+          <div className="col-start-3  z-20  self-center justify-self-center overlap">
+            <figure className="flex">
+              <Image
+                src="/assets/play-button.png"
+                alt="Video"
+                layout="intrinsic"
+                width={80}
+                height={91}
+              />
+              <figcaption className="pt-1 text-base text-primary-link">
+                <span className="text-shadow">zobacz film</span>
+              </figcaption>
+            </figure>
+          </div>
+          <figure className="relative col-start-3 self-center justify-self-center overlap text-shadow">
+            <Image
+              className="z-10"
+              src="/assets/video_thumbnail.png"
+              alt="Video"
+              layout="intrinsic"
+              width={640}
+              height={482}
+            />
+            <div
+              className="z-0 absolute gradient-box h-5/3 w-96 -top-1/2
+            left-1/2 transform -translate-x-1/2"
+            ></div>
+          </figure>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+export default Home
