da6ccaa3837a7ab386dce6ed84a31d5cd53f42b2
Igor Wysocki <narghar@gmail.com>
2021-05-28 11:53:02 +0200

    Add form validation

--- pages/kontakt/index.js
+++ pages/kontakt/index.js
@@ -1,6 +1,51 @@
+import { useFormik } from "formik";
 import Head from "next/head";
+import React from "react";
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = "Required";
+  } else if (values.name.length > 15) {
+    errors.name = "Must be 15 characters or less";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  if (!values.tel) {
+    errors.tel = "Required";
+  } else if (values.tel.length < 9) {
+    errors.tel = "Must be 9 characters or more";
+  }
+
+   if (!values.message) {
+     errors.message = "Required";
+   } else if (values.message.length < 20) {
+     console.log(values.message)
+     errors.message = "Must be 20 characters or more";
+   }
+  console.log(errors)
+  return errors;
+};
 
 function Contact() {
+   const formik = useFormik({
+     initialValues: {
+       name: "",
+       email: "",
+       tel: "",
+       message: "",
+     },
+     validate,
+     onSubmit: (values) => {
+       alert(JSON.stringify(values, null, 2));
+     },
+   });
   return (
     <div>
       <Head>
@@ -8,7 +53,7 @@ function Contact() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="container w-96 mx-auto">
-        <form className="pt-12" action="/kontakt" method="GET">
+        <form onSubmit={formik.handleSubmit} className="pt-12">
           <fieldset>
             <legend>Formularz kontaktowy</legend>
 
@@ -21,9 +66,11 @@ function Contact() {
                 id="name"
                 placeholder="Wprowadź imię"
                 pattern="[a-zA-Z ]+"
-                minlength="3"
+                minLength="3"
                 title="Tylko litery"
                 required
+                onChange={formik.handleChange}
+                value={formik.values.name}
               />
             </div>
 
@@ -36,6 +83,8 @@ function Contact() {
                 id="email"
                 required
                 placeholder="Wprowadź e-mail"
+                onChange={formik.handleChange}
+                value={formik.values.email}
               />
             </div>
 
@@ -46,12 +95,14 @@ function Contact() {
                 type="text"
                 name="tel"
                 id="tel"
-                minlength="9"
-                maxlength="12"
+                minLength="9"
+                maxLength="12"
                 placeholder="Wprowadź tel"
                 pattern="[0-9]+"
                 title="Tylko cyfry od 1-9"
                 required
+                onChange={formik.handleChange}
+                value={formik.values.tel}
               />
             </div>
 
@@ -63,16 +114,13 @@ function Contact() {
               cols="40"
               placeholder="Wprowadź wiadomość"
               required
+              onChange={formik.handleChange}
+              value={formik.values.message}
             ></textarea>
 
-
-              <button
-                className=""
-                type="submit"
-                id="submit"
-              >Wyślij
-              </button>
-
+            <button className="" type="submit" id="submit">
+              Wyślij
+            </button>
           </fieldset>
         </form>
       </div>
